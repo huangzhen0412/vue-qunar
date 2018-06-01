1e6f2d57a06919491db052352a98d291005af4b9
huangzhen <305325375@qq.com>
2018-06-02 00:42:30 +0830

    add home-swiper

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -21,9 +21,10 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+	@import '~styles/varibles.styl'
 	.header
 		line-height .86rem
-		background #00bcd4
+		background $bgColor
 		display flex
 		color #fff
 		.header-left
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,44 @@
+<template>
+	<swiper :options="swiperOption">
+	    <swiper-slide v-for="item of swiperList" :key="item.id">
+	    	<img :src="item.imgUrl" class="swiper-img">
+	    </swiper-slide>
+	    <div class="swiper-pagination" slot="pagination"></div>
+  	</swiper>
+</template>
+
+<script>
+export default {
+	name: 'HomeSwiper',
+	data () {
+		return {
+			swiperOption: {
+				pagination: {
+					el: '.swiper-pagination'
+				}
+			},
+			swiperList: [
+				{
+					id: '0001',
+					imgUrl: 'http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg'
+				},
+				{
+					id: '0002',
+					imgUrl: 'http://img1.qunarzz.com/piao/fusion/1802/42/7c92b9a381e46402.jpg_640x200_1cdce2a4.jpg'
+				},
+				{
+					id: '0003',
+					imgUrl: 'http://img1.qunarzz.com/piao/fusion/1802/51/e78f936a5b404102.jpg_640x200_c14f0b3a.jpg'
+				}
+			]	
+		}
+	}
+}
+</script>
+
+<style lang="stylus" scoped>
+	>>> .swiper-pagination-bullet-active
+		background #fff
+	.swiper-img
+		width 100%
+</style>
