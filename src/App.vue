d8e4f84ef8ed6f237aebdcdba0943b2552e726e9
huangzhen <305325375@qq.com>
2018-06-01 23:24:49 +0830

    project initialized

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" class="app-img">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="stylus" scoped>
+  #app 
+    text-align center
+</style>
