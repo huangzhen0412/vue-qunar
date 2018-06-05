ad35b634b26dc65e81038d1f8ebf19d9641706dd
huangzhen <305325375@qq.com>
2018-06-05 22:04:45 +0830

    add city-list

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,17 +2,55 @@
 	<div>
 		<city-header></city-header>
 		<city-search></city-search>
+		<city-list :hotCities="hotCities" :cities="cities" :letter="letter"></city-list>
+		<city-alphabet 
+			:cities="cities"
+			v-on:change="handleLetterChange"
+		>	
+		</city-alphabet>
 	</div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlphabet from './components/Alphabet'
+import axios from 'axios'
 export default {
 	name: 'City',
 	components: {
 		CityHeader,
-		CitySearch
+		CitySearch,
+		CityList,
+		CityAlphabet
+	},
+	data () {
+		return {
+			cities: {},
+			hotCities: [],
+			letter: ''
+		}
+	},
+	methods: {
+		getCityInfo () {
+			axios.get('/api/city.json')
+				.then(this.getCityInfoSucc)
+		},
+		getCityInfoSucc (res) {
+			res = res.data
+			if(res.ret && res.data){
+				const data = res.data
+				this.hotCities = data.hotCities
+				this.cities = data.cities
+			}
+		},
+		handleLetterChange (letter) {
+			this.letter = letter
+		}
+	},
+	mounted () {
+		this.getCityInfo()
 	}
 }
 </script>
